ccb7a0b96b7cdcb5907bbc05dc876e7b441e7f82
George Gaylor <georgegaylor83@outlook.com>
2017-09-19 02:53:47 -0700

    challenge complete

---
+++ LinkedLists/LinkedLists/LinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LinkedLists
+{
+    class LinkedList
+    {
+        public Node head;
+
+        public void Add(string newData)
+        {
+            Node toAdd = new Node
+            {
+                data = newData
+            };
+            if (head == null)
+            {
+                toAdd.next = head;
+                head = toAdd;
+            }
+            else
+            {
+                Node current = head;
+
+
+               Console.WriteLine(current.data);
+                while (current.next != null)
+                {
+                    current = current.next;
+                    Console.WriteLine(current.data);
+                }
+                current.next = toAdd;
+            }
+        }
+        public void PrintList()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("list is empty");
+            }
+            else
+            {
+                Node current = head;
+                Console.WriteLine(current.data);
+                while (current.next != null)
+                {
+                    current = current.next;
+                    Console.WriteLine(current.data);
+                }
+            }
+        }
+    }
+}
---
+++ LinkedLists/LinkedLists/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LinkedList newlist = new LinkedList();
+            string x = "what??";
+            string y = "morestuff";
+            string z = "evenmore";
+            newlist.Add(y);
+            newlist.Add(z);
+            newlist.Add(x);
+            newlist.PrintList();
+            Console.Read();
+        }
+    }
+}
