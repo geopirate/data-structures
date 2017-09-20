3a227933893010ab7b960e342055b13f43f90401
George Gaylor <georgegaylor83@outlook.com>
2017-09-19 17:27:41 -0700

    Challenge complete

---
+++ LinkedListMiddle/LinkedListMiddle/LinkedList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace LinkedListMiddle
+{
+    class LinkedList
+    {
+        public Node head;
+        public int count;
+        public int middle;
+
+        public void Add(string newData)
+        {
+            Node toAdd = new Node(newData);
+
+            if (head == null)
+            {
+                toAdd.next = head;
+                head = toAdd;
+            }
+            else
+            {
+                Node current = head;
+
+                //Console.WriteLine(current.data);
+                while (current.next != null)
+                {
+                    current = current.next;
+                    //Console.WriteLine(current.data);
+                }
+                current.next = toAdd;
+            }
+            count++;
+            if (count % 2 == 1) {
+                middle++;
+            };
+
+        }
+        public void PrintList()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("list is empty");
+            }
+            else
+            {
+                Node current = head;
+                Console.WriteLine(current.data);
+                while (current.next != null)
+                {
+                    current = current.next;
+                    Console.WriteLine(current.data);
+                }
+            }
+        }
+
+        public void PrintMiddle()
+        {
+            int listIndex = 1;
+            if (head == null)
+            {
+                Console.WriteLine("The list is empty.");
+            }
+            else
+            {
+                Node current = head;
+                while(listIndex < middle)
+                {
+                    current = current.next;
+                    listIndex++;
+                }
+                Console.WriteLine(current.data);
+            }
+        }
+
+    }
+}
---
+++ LinkedListMiddle/LinkedListMiddle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LinkedListMiddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList newlist = new LinkedList();
+
+            newlist.Add("first");
+            newlist.Add("second");
+            newlist.Add("third");
+            newlist.Add("fourth");
+            newlist.Add("fifth");
+            newlist.Add("sixth");
+            newlist.Add("seventh");
+            newlist.Add("eighth");
+            newlist.Add("ninth");
+            newlist.PrintList();
+
+            Console.WriteLine("Middle is " + newlist.middle + " of " + newlist.count);
+
+
+            Console.WriteLine();
+            newlist.PrintMiddle();
+            Console.WriteLine();
+
+
+
+            Console.Read();
+        }
+    }
+}
