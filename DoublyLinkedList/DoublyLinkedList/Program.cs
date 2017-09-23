1954930e5086ef29bde12ee2b6a61f7ab722aca7
George Gaylor <georgegaylor83@outlook.com>
2017-09-22 23:00:29 -0700

    AddComplete

---
+++ DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList newlist = new LinkedList();
+
+            newlist.Append("first");
+            newlist.Append("second");
+            newlist.Append("third");
+            newlist.Append("fourth");
+            newlist.Append("fifth");
+            newlist.Append("sixth");
+            newlist.Append("seventh");
+            newlist.Append("eighth");
+            newlist.Append("ninth");
+
+            newlist.PrintList();
+            Console.WriteLine();
+
+            newlist.DeleteData("eighth");
+
+            Console.WriteLine();
+            newlist.PrintList();
+
+            Console.Read();
+        }
+    }
+}
