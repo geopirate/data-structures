9dcc17764e85dea28ddca34c74e795127b0b9536
George Gaylor Jr <georgegaylor83@gmail.com>
2017-09-23 01:58:08 -0700

    Fixed Delete

--- DoublyLinkedList/DoublyLinkedList/LinkedList.cs
+++ DoublyLinkedList/DoublyLinkedList/LinkedList.cs
@@ -45,8 +45,6 @@ namespace DoublyLinkedList
             toAdd.Previous = current;
         }
 
-        // insert
-
         // delete
         public void DeleteData(Object z)
         {
@@ -57,97 +55,22 @@ namespace DoublyLinkedList
                 
                 if (current.Next.Data == z)
                 {
+                    // deletes the last node
                     if (current.Next.Next == null)
                     {
-                        current.Next = current.Next.Next;
-                    }
-
-                    else if (current.Next.Next == null)
-                    {
-                        current.Next = current.Next.Next;
-                    }
+                        current.Next.Previous = null;
+                        current.Next = null;
+                    } // deletes anything else
                     else
                     {
-                        
+                        current.Next.Next.Previous = current;
+                        current.Next = current.Next.Next;
                     }
-                    
-                    current.Next.Next.Previous = current;
                     //Console.WriteLine(z + " is deleted.");
                     return;
                 }
                 current = current.Next;
             }
         }
-        /*
-        public void PrintMiddle()
-        {
-            /*int listIndex = 1;
-            if (head == null)
-            {
-                Console.WriteLine("The list is empty.");
-            }
-            else
-            {
-                Node current = head;
-                while (listIndex < middle)
-                {
-                    current = current.Next;
-                    listIndex++;
-                }
-                Console.WriteLine(current.Data);
-            }
-        }
-
-        // find the data of the node x from the end
-        public void PrintRandom(int x)
-        {
-            int listIndex = 0;
-            if (head == null)
-            {
-                Console.WriteLine("The list is empty.");
-            }
-            else
-            {
-                Node current = head;
-                Node random = head;
-                while (current.Next != null)
-                {
-                    current = current.Next;
-                    listIndex++;
-                    if (listIndex > x)
-                    {
-                        random = random.Next;
-                    }
-                }
-                Console.WriteLine();
-                Console.WriteLine($"The node {random.Data} is {x} from the end");
-            }
-        }
-
-        public void FindData(Object z)
-        {
-            int listIndex = 0;
-            int foundIndex;
-            if (head == null)
-            {
-                Console.WriteLine("list is empty");
-            }
-            else
-            {
-                Node current = head;
-                while (current.Next != null)
-                {
-                    if (current.Data == z)
-                    {
-                        foundIndex = listIndex;
-                    }
-                    current = current.Next;
-
-                    listIndex++;
-                }
-                Console.WriteLine(z + " is at node " + listIndex + ".");
-            }
-        }*/
-
     }
 }
