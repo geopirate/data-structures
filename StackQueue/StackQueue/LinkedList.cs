9a11b48044b3d92b81c93f9f53c5e8d78902b99e
George Gaylor <georgegaylor83@outlook.com>
2017-09-26 21:25:08 -0700

    Challenge Complete!

---
+++ StackQueue/StackQueue/LinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueue
+{
+    class LinkedList
+    {
+        public Node head = new Node(null);
+
+        public Node FindTail()
+        {
+            Node current = head;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            return current;
+        }
+
+        public void PrintList()
+        {
+            if( head == null)
+            {
+                Console.WriteLine("This is empty!");
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                    Console.Write($"{current.Data} => ");
+                }
+            }
+        }
+
+        
+    }
+}
