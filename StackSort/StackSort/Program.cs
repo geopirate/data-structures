1c0238a26df6f6346505d0d2036d32f87065b8cc
George Gaylor <georgegaylor83@outlook.com>
2017-09-29 22:17:12 -0700

    challenge complete

---
+++ StackSort/StackSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StackSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This adds numbers to a stack and sorts them on push.");
+
+            Stack sorter = new Stack();
+
+            sorter.Push(9);
+            sorter.Push(2);
+            sorter.Push(3);
+            sorter.Push(4);
+            sorter.Push(5);
+            sorter.Push(8);
+            sorter.Push(7);
+
+            sorter.Print();
+            Console.WriteLine("Add a number before the first element.");
+            sorter.Push(1);
+            sorter.Print();
+            Console.WriteLine("Add a number in the middle.");
+            sorter.Push(6);
+            sorter.Print();
+            Console.WriteLine("Add a number after the last element.");
+            sorter.Push(10);
+            sorter.Print();
+
+            Console.WriteLine("Pop, no sorting required!");
+            sorter.Pop();
+            sorter.Print();
+
+            Console.Read();
+        }
+    }
+}
---
+++ StackSort/StackSort/Stack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace StackSort
+{
+    class Stack
+    {
+        Node Head;
+        Node Tail;
+
+        public Stack()
+        {
+            Head = new Node(0);
+            Tail = new Node(0);
+
+            Head.Next = Tail;
+            Tail.Previous = Head;
+        }
+
+        public void Push(int newData)
+        {
+            Node newNode = new Node(newData); 
+            Node current = Sort(newData);
+
+            newNode.Next = current;
+            newNode.Previous = current.Previous;
+            current.Previous.Next = newNode;
+            current.Previous = newNode;
+
+        }
+
+        public void Pop()
+        {
+            Head.Next.Next.Previous = Head;
+            Head.Next = Head.Next.Next;
+        }
+
+        public Node Sort(int data)
+        {
+            Node current = Head.Next;
+            if(current == null)
+            {
+                return Tail;
+            }
+
+            while (data > current.Data)
+            {
+                if(current.Data == 0)
+                {
+                    return Tail;
+                }
+                current = current.Next;
+            }
+            return current;
+        }
+
+        public void Print()
+        {
+            if (Head.Next == null)
+            {
+                Console.WriteLine("list is empty");
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next.Data != 0)
+                {
+                    current = current.Next;
+                    Console.Write($"{ current.Data} => ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
