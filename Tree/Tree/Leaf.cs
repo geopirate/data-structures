d3135bf4ae1b3949cdc8079804729ea37c1aed02
George Gaylor <georgegaylor83@outlook.com>
2017-10-02 17:45:53 -0700

    Tree

---
+++ Tree/Tree/Leaf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Leaf
+    {
+        // the value inside the leaf
+        public int Value;
+        // the left child leaf
+        public Leaf Left;
+        // the right child leaf
+        public Leaf Right;
+
+        // leaf constructor that creates a leaf using a value input and sets children to null
+        public Leaf (int leafValue)
+        {
+            Value = leafValue;
+            Left = null;
+            Right = null;
+        }
+    }
+}
---
+++ Tree/Tree/Tree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Tree
+    {
+        // value of the root leaf (node?)
+        public Leaf Root;
+
+        // initializes the tree with a root node
+        public Tree()
+        {
+            Root = new Leaf(0);
+        }
+        /* started working on an add method, but this still needs some work
+        public void FindLeaf(int childValue)
+        {
+            Leaf current = Root;
+
+            bool searching = true;
+
+            while (searching)
+            {
+                if(current.Left != null && current.Right != null)
+                {
+                    current = current.Left;
+                }
+                else if(current.Left == null)
+                {
+                    Add(current, childValue);
+                }
+                else if(current.Right == null)
+                {
+                    Add(current, childValue);
+                }
+            }
+        }
+
+        public void Add(Leaf parent, int child)
+        {
+            if (parent.Left == null)
+            {
+                parent.Left = new Leaf(child);
+            }
+            else if (parent.Right == null)
+            {
+                parent.Right = new Leaf(child);
+            }
+            else
+            {
+                return;
+            }
+        }*/
+    }
+}
