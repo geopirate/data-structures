6805b5b2ab7d3f55f0460bd5a6ced0133b1c1e9a
George Gaylor <georgegaylor83@outlook.com>
2017-10-09 02:12:14 -0700

    Removed empty head and tail nodes and refactored

--- StackSort/StackSort/Stack.cs
+++ StackSort/StackSort/Stack.cs
@@ -9,66 +9,108 @@ namespace StackSort
 
         public Stack()
         {
-            Head = new Node(0);
-            Tail = new Node(0);
-
-            Head.Next = Tail;
-            Tail.Previous = Head;
+            Head = null;
+            Tail = null;
         }
 
-        public void Push(int newData)
+        public Node Pop()
         {
-            Node newNode = new Node(newData); 
-            Node current = Sort(newData);
+            Node current = Head;
 
-            newNode.Next = current;
-            newNode.Previous = current.Previous;
-            current.Previous.Next = newNode;
-            current.Previous = newNode;
-
-        }
-
-        public void Pop()
-        {
-            Head.Next.Next.Previous = Head;
-            Head.Next = Head.Next.Next;
-        }
-
-        public Node Sort(int data)
-        {
-            Node current = Head.Next;
-            if(current == null)
+            if (current == null)
+            { // empty stack
+                return null;
+            } // one left in stack
+            else if (current.Next == null)
             {
-                return Tail;
+                Head = null;
+                Tail = null;
             }
-
-            while (data > current.Data)
+            else
             {
-                if(current.Data == 0)
-                {
-                    return Tail;
-                }
-                current = current.Next;
+                Head = current.Next;
+                current.Next = null;
             }
             return current;
         }
 
+        public void Push(int newData)
+        {
+            Node newNode = new Node(newData);
+            Node Current = Head;
+            // if stack is empty
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+                return;
+            } // if stack only has one item
+            else if (Head.Next == null)
+            { // if new is smaller
+                if (newData < Head.Data)
+                {
+                    Head = newNode;
+                    newNode.Next = Current;
+                    Current.Previous = newNode;
+                    return;
+                } // if new is bigger
+                else
+                {
+                    Tail = newNode;
+                    newNode.Previous = Current;
+                    Current.Next = newNode;
+                    return;
+                }
+            }
+            // we have at least 2 nodes, so we sort
+            // if new is the smallest make it the new head
+            if(newData <= Head.Data)
+            {
+                Head.Previous = newNode;
+                newNode.Next = Head;
+                Head = newNode;
+                return;
+            }// if new is the biggest make it the new tail
+            if (newData >= Tail.Data)
+            {
+                Tail.Next = newNode;
+                newNode.Previous = Tail;
+                Tail = newNode;
+                return;
+            }// otherwise find where to insert it
+            while (Current != Tail)
+            {
+                if(newData < Current.Data)
+                {
+                    newNode.Next = Current;
+                    newNode.Previous = Current.Previous;
+                    Current.Previous.Next = newNode;
+                    Current.Previous = newNode;
+                    return;
+                }
+               Current = Current.Next;
+            }
+        }
         public void Print()
         {
-            if (Head.Next == null)
+            if (Head == null)
             {
-                Console.WriteLine("list is empty");
+                Console.WriteLine("List is empty!");
             }
             else
             {
                 Node current = Head;
-                while (current.Next.Data != 0)
+                while (current != null)
                 {
-                    current = current.Next;
                     Console.Write($"{ current.Data} => ");
+                    current = current.Next;
                 }
                 Console.WriteLine();
             }
         }
     }
 }
+/* The sorting works, but the initial head and tail nodes which are initialized with a zero value are unnecessary. 
+ * The head and tail should be initialized to null and set when a value is pushed into the stack. 
+ * Currently, the head and the tail are disconnected. 
+ */
