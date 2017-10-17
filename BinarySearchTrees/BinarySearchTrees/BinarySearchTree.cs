aa73b68440078322ccb5b7b9e61f480a175fbc39
George Gaylor <georgegaylor83@outlook.com>
2017-10-16 19:54:03 -0700

    Added traversal methods and created program code

--- BinarySearchTrees/BinarySearchTrees/BinarySearchTree.cs
+++ BinarySearchTrees/BinarySearchTrees/BinarySearchTree.cs
@@ -10,48 +10,91 @@ namespace BinarySearchTrees
         public Node Root;
 
         // initializes the tree with a root node
-        public BinarySearchTree() => Root = null;
+        public BinarySearchTree(int initialRoot) => Root = new Node(initialRoot);
 
-        public void BSTAdd(int data)
+        public void BSTAdd(Node current, int data)
         {
-            if (Root == null)
-                Root = new Node(data);
-
-            Node current = Root;
-
-            while (current != null)
+            // left is less
+            if (data < current.Data)
             {
-                // left is less
-                if (data < current.Data)
+                if (current.Left == null)
                 {
-                    if(current.Left == null)
-                    {
-                        current.Left = new Node(data);
-                        return;
-                    }
-                    else
-                    {
-                        BSTAdd(current.Left.Data);
-                        return;
-                    }
+                    current.Left = new Node(data);
+                    return;
                 }
-                // right is more 
-                if (data > current.Data)
+                else
                 {
-                    if (current.Right == null)
-                    {
-                        current.Right = new Node(data);
-                        return;
-                    }
-                    else
-                    {
-                        BSTAdd(current.Right.Data);
-                        return;
-                    }
+                    BSTAdd(current.Left, data);
+                    return;
                 }
             }
-
+            // right is more 
+            if (data > current.Data)
+            {
+                if (current.Right == null)
+                {
+                    current.Right = new Node(data);
+                    return;
+                }
+                else
+                {
+                    BSTAdd(current.Right, data);
+                    return;
+                }
+            }
         }
+
+        public void BreadthOrder(Node Current)
+        {
+            Queue<Node> tree = new Queue<Node>();
+
+            if (Current == null) { return; }
+
+            tree.Enqueue(Current);
+
+            while (tree.Count > 0)
+            {
+                Node Counter = tree.Dequeue();
+                if (Counter.Left != null)
+                {
+                    tree.Enqueue(Counter.Left);
+                }
+                if (Counter.Right != null)
+                {
+                    tree.Enqueue(Counter.Right);
+                }
+                Console.Write($"{Counter.Data} ");
+            }
+        }
+
+        public void PreOrder(Node Current)
+        {
+            if (Current != null)
+            {
+                Console.Write($"{Current.Data} ");
+                PreOrder(Current.Left);
+                PreOrder(Current.Right);
+            }
+        }
+        public void PostOrder(Node Current)
+        {
+            if (Current != null)
+            {
+                PostOrder(Current.Left);
+                PostOrder(Current.Right);
+                Console.Write($"{Current.Data} ");
+            }
+        }
+        public void InOrder(Node Current)
+        {
+            if (Current != null)
+            {
+                InOrder(Current.Left);
+                Console.Write($"{Current.Data} ");
+                InOrder(Current.Right);
+            }
+        }
+
     }
     // the node class in our binary search tree
     public class Node
