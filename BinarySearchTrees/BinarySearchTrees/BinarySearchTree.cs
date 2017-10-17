b3df4a46a2ad16a64727ef1b240a18bc9c4de100
George Gaylor <georgegaylor83@outlook.com>
2017-10-16 17:04:13 -0700

    Initial implementation of whiteboard

---
+++ BinarySearchTrees/BinarySearchTrees/BinarySearchTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTrees
+{
+    public class BinarySearchTree
+    {
+        // value of the root leaf (node?)
+        public Node Root;
+
+        // initializes the tree with a root node
+        public BinarySearchTree() => Root = null;
+
+        public void BSTAdd(int data)
+        {
+            if (Root == null)
+                Root = new Node(data);
+
+            Node current = Root;
+
+            while (current != null)
+            {
+                // left is less
+                if (data < current.Data)
+                {
+                    if(current.Left == null)
+                    {
+                        current.Left = new Node(data);
+                        return;
+                    }
+                    else
+                    {
+                        BSTAdd(current.Left.Data);
+                        return;
+                    }
+                }
+                // right is more 
+                if (data > current.Data)
+                {
+                    if (current.Right == null)
+                    {
+                        current.Right = new Node(data);
+                        return;
+                    }
+                    else
+                    {
+                        BSTAdd(current.Right.Data);
+                        return;
+                    }
+                }
+            }
+
+        }
+    }
+    // the node class in our binary search tree
+    public class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+
+        public Node(int newData)
+        {
+            Data = newData;
+            Left = null;
+            Right = null;
+        }
+    }
+}
