ec856eec62b057c2ce0de54b7c279cd7486fcfa2
George Gaylor <georgegaylor83@outlook.com>
2017-10-20 19:19:10 -0700

    Challenge mostly complete
    forgot to do intermediary commmits until it was almost done

---
+++ AnimalGuessGame/AnimalGuessGame/AnimalGuessGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AnimalGuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = new string[] {
+                "cat",
+                "Does it meow?",
+                "dog",
+                "Is it a Mammal?",
+                "fish",
+                "Does it swim?",
+                "snake"
+            };
+            BST myTree = new BST(myArray);
+            myTree.AskQuestion(myTree.Root);
+
+            //Console.WriteLine("This traverses a binary tree Breadth Order.");
+            //myTree.BreadthOrder(myTree.Root);
+
+            Console.Read();
+        }
+    }
+}
