a044d64677e8d07c610a50add6c8a5a7b198fab8
George Gaylor <georgegaylor83@outlook.com>
2017-10-24 23:05:55 -0700

    Challenge not complete

---
+++ SortingAlgorithms/SortingAlgorithms/SortingAlgorithms/MergeReadMe.md
@@ -0,0 +1,14 @@
+What did you learn?
+I learned a lot about trying to implement a merge sort, even if we weren't successful.  I was also able to teach my partner a lot about data structures in general, this took a good chunk of time today....
+Explain how you felt before starting the challenge, what did you learn while researching, and how well do you understand the data structure after your self research
+I felt this would be a pretty challenging data structure to implement.  I understand it betrter than when I started, but not completely as we did not come up with a final solution and what we have so far diverges from the model solution.
+Where did you look?
+I just read the first couple paragraphs of the reference material provided, to figure out what we are supposed to do and worked through the challenge time before looking at the code when I got home.
+Be specific, what sites? resources? Books?
+See previous answer.
+What notes did you use to help you successfully complete the challenge?
+We just took notes on the whiteboard.
+Did you write anything down, bookmark anything that looked useful?
+No, see previous answeers.
+What did you use as a guideline or direction during the challenge?)
+We mostly used each other as references as we worked through the problem and iteratively tried to craft a solution.  We also worked with the TAs especially Nathan.
---
+++ SortingAlgorithms/SortingAlgorithms/SortingAlgorithms/MergeSort.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    class MergeSort
+    {
+        public int[] arr;
+
+        public MergeSort(int[] newArray)
+        {
+            arr = newArray;
+        }
+
+        public int[] Sort()
+        {
+            int [] thisArray = arr;
+
+            DivideDrop(thisArray);
+
+            return thisArray;
+        }
+
+        private int[] DivideDrop(int[] w)
+        {
+            int v = w.Length / 2;
+            int[] newArray1 = new int[v];
+            int[] newArray2 = new int[w.Length-v];
+
+            for(int i=0; i<v; i++)
+                newArray1[i] = w[i];
+            Console.WriteLine("\nUnsorted Array1: ");
+            Print(newArray1);
+
+            int u = 0;
+            for(int i = v; i < w.Length; i++)
+            {
+                newArray2[u] = w[i];
+                u++;
+            }
+            Console.WriteLine("\nUnsorted Array2: ");
+            Print(newArray2);
+
+            return w;
+        }
+
+        private int[] Merge(int[]x, int[] y)
+        {
+            int[] z = new int[x.Length + y.Length];
+            int zIndex = 0;
+            int i = 0;
+            int k = 0;
+
+            for (i = 0; i < x.Length; i++)
+            {
+                for(k=0; k<y.Length; k++)
+                {
+                    if(y[k] < x[i])
+                    {
+                        z[zIndex] = y[k];
+                        zIndex++;
+                    } else
+                    {
+                        z[zIndex] = x[i];
+                        zIndex++;
+                    }
+                }
+
+            }
+
+            return z;
+        }
+
+        public void Print(int[] printedArray)
+        {
+            foreach (int x in printedArray)
+            {
+                Console.Write($" {x}");
+            }
+        }
+    }
+}
