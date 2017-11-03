de983804a2b7c44c6526d02cf042f22d783e3dae
George Gaylor <georgegaylor83@outlook.com>
2017-11-02 21:30:56 -0700

    Whiteboard Code added

---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Rotate rotator = new Rotate(5);
+
+            Console.WriteLine("Starter: ");
+            rotator.Print();
+            Console.WriteLine("\nRotated: ");
+            rotator.Rotator();
+            rotator.Print();
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Matrix/Matrix/Rotate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    class Rotate
+    {
+        int[,] matrix;
+        int x = 0;
+        int y;
+
+        public Rotate(int n)
+        {
+            int k = 1;
+            y = n - 1;
+            matrix = new int[n,n];
+            for(int i = 0; i<n; i++)
+            {
+                for(int j = 0; j<n; j++)
+                {
+                    matrix[i, j] = k;
+                    k++;
+                }
+            }
+        }
+
+        public void Rotator()
+        {
+            int k = y;
+       
+            for(int i  = x; i < y; i++)
+            {
+                int temp = matrix[x, i];
+                matrix[x, i] = matrix[k, x];
+                matrix[k, x] = matrix[y, k];
+                matrix[y, k] = matrix[i, y];
+                matrix[i, y] = temp;
+                k--;
+            }
+        }
+
+        public void Print()
+        {
+            for(int d = 0; d <=y; d++)
+            {
+                for(int f = 0; f <=y; f++)
+                {
+                    Console.Write($"{matrix[d,f]}, ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
