b07d6acd4f13faff29f963562bbb7d9f568e05c7
George Gaylor <georgegaylor83@outlook.com>
2017-11-02 23:23:05 -0700

    Finished getting code working and added comments

--- Matrix/Matrix/Program.cs
+++ Matrix/Matrix/Program.cs
@@ -6,15 +6,20 @@ namespace Matrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Rotate rotator = new Rotate(5);
+            Console.WriteLine("Given an NxN matrix write a method to rotate the image by 90 degrees.");
+            Console.WriteLine("Can you do this in-place ?                            You're welcome.");
+            // change the n value to change the size of the matrix
+            // Amy asked for a 50x50 but it doesn't print well.....
+            int n = 9;
+            // creates a matrix using an iterator starting at 11 of size n x n as requested
+            Rotate rotator = new Rotate(n);
 
-            Console.WriteLine("Starter: ");
+            Console.WriteLine("\nStarter: ");
             rotator.Print();
+
             Console.WriteLine("\nRotated: ");
             rotator.Rotator();
             rotator.Print();
-
 
             Console.Read();
         }
--- Matrix/Matrix/Rotate.cs
+++ Matrix/Matrix/Rotate.cs
@@ -6,14 +6,16 @@ namespace Matrix
 {
     class Rotate
     {
+        // the matrix to be rotated
         int[,] matrix;
-        int x = 0;
-        int y;
+        // keeping track of the last index based on n
+        static int lastIndex;
 
+        // constructor that creates the matrix for rotation
         public Rotate(int n)
         {
-            int k = 1;
-            y = n - 1;
+            int k = 11;
+            lastIndex = n - 1;
             matrix = new int[n,n];
             for(int i = 0; i<n; i++)
             {
@@ -27,24 +29,43 @@ namespace Matrix
 
         public void Rotator()
         {
-            int k = y;
-       
-            for(int i  = x; i < y; i++)
+            // x is the lower bounds of the current round of rotation
+            int x = 0;
+            // y is the upper bounds of the current round of rotation
+            int y = lastIndex;
+
+            // l keeps track of how many rounds of rotation are needed
+            for (int l = 0; l < lastIndex / 2; l++)
             {
-                int temp = matrix[x, i];
-                matrix[x, i] = matrix[k, x];
-                matrix[k, x] = matrix[y, k];
-                matrix[y, k] = matrix[i, y];
-                matrix[i, y] = temp;
-                k--;
+                // k is the upper bounds of the current digit of rotation
+                int k = y;
+                // i is the lower bounds of the current digit of rotation
+                for (int i = x; i < y; i++)
+                {
+                    // store top left in a temp variable
+                    int temp = matrix[x, i];
+                    // replace top left with bottom left
+                    matrix[x, i] = matrix[k, x];
+                    // replace bottom left with bottom right
+                    matrix[k, x] = matrix[y, k];
+                    // replace bottom right with top right
+                    matrix[y, k] = matrix[i, y];
+                    // replace top right with the temp we saved from top left initially
+                    matrix[i, y] = temp;
+                    // decrementing this and incrementing i moves over to the next digit
+                    k--;
+                }
+                // these move us a row deeper into the matrix
+                x++;
+                y--;
             }
         }
 
         public void Print()
         {
-            for(int d = 0; d <=y; d++)
+            for(int d = 0; d <= lastIndex; d++)
             {
-                for(int f = 0; f <=y; f++)
+                for(int f = 0; f <= lastIndex; f++)
                 {
                     Console.Write($"{matrix[d,f]}, ");
                 }
