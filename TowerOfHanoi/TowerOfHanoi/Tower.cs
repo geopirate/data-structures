76aa3c739aa62fd2da08b2831b225963f9b996e3
George Gaylor <georgegaylor83@outlook.com>
2017-11-06 20:54:52 -0800

    typos

--- TowerOfHanoi/TowerOfHanoi/Tower.cs
+++ TowerOfHanoi/TowerOfHanoi/Tower.cs
@@ -6,5 +6,45 @@ namespace TowerOfHanoi
 {
     class Tower
     {
+        Stack<Disk> Left = new Stack<Disk>();
+        Stack<Disk> Middle = new Stack<Disk>();
+        Stack<Disk> Right = new Stack<Disk>();
+
+        public Tower(int n)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                Left.Push(new Disk(i));
+            }
+
+            if(n%2 == 0)
+            {
+                ShiftEven();
+            } else
+            {
+                ShiftOdd();
+            }
+        }
+
+        private void ShiftOdd()
+        {
+            if
+        }
+
+        private void ShiftEven()
+        {
+
+        }
+
+    }
+
+    class Disk
+    {
+        int value;
+
+        public Disk(int newValue)
+        {
+            value = newValue;
+        }
     }
 }
