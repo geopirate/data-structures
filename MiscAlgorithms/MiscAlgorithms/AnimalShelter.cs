da3fe712f85290a49513bfdab8df89a88a0b7dd5
George Gaylor <georgegaylor83@outlook.com>
2017-11-07 17:43:50 -0800

    initial code from whiteboard and picture

---
+++ MiscAlgorithms/MiscAlgorithms/AnimalShelter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiscAlgorithms
+{
+    class AnimalShelter
+    {
+        int TotalArrivals = 1;
+        Queue<Animal> Cat = new Queue<Animal>();
+        Queue<Animal> Dog = new Queue<Animal>();
+
+        public void Load(string type)
+        {
+            if (type == "cat")
+                Cat.Enqueue(new Animal(type, TotalArrivals));
+            else
+                Dog.Enqueue(new Animal(type, TotalArrivals));
+            TotalArrivals++;
+            Print();
+        }
+
+        public Animal Adopt(string input)
+        {
+            if(input == "cat")
+                return Cat.Dequeue();
+            if (input == "dog")
+                return Dog.Dequeue();
+            else
+                if (Cat.Peek().Arrival < Dog.Peek().Arrival)
+                    return Cat.Dequeue();
+                else
+                    return Dog.Dequeue();
+        }
+
+        public void Print()
+        {
+            Console.Write("\nCats : ");
+            foreach ( Animal x in Cat)
+            {
+                Console.Write($"{x.Arrival}-{x.Type} ");
+            }
+            Console.Write("\nDogs : ");
+            foreach (Animal x in Dog)
+            {
+                Console.Write($"{x.Arrival}-{x.Type} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Animal
+    {
+        public string Type;
+        public int Arrival;
+
+        public Animal(string type, int arrival)
+        {
+            Type = type;
+            Arrival = arrival;
+        }
+    }
+}
--- MiscAlgorithms/MiscAlgorithms/Program.cs
+++ MiscAlgorithms/MiscAlgorithms/Program.cs
@@ -6,20 +6,43 @@ namespace MiscAlgorithms
     {
         static void Main(string[] args)
         {
-            AssociativeArray newAA = new AssociativeArray();
-            State wash = new State("WA", "Olympia");
+            //AssociativeArray newAA = new AssociativeArray();
+            //State wash = new State("WA", "Olympia");
 
-            newAA.Add(new State("OR", "Salem"));
-            newAA.Add(wash);
-            newAA.Add(new State("TX", "Austin"));
-            newAA.Add(new State("NY", "Albany"));
-            newAA.SearchKey("TX");
+            //newAA.Add(new State("OR", "Salem"));
+            //newAA.Add(wash);
+            //newAA.Add(new State("TX", "Austin"));
+            //newAA.Add(new State("NY", "Albany"));
+            //newAA.SearchKey("TX");
+            //newAA.Print();
+            //newAA.Delete(wash);
+            //newAA.Print();
 
-            newAA.Print();
+            AnimalShelter shelter = new AnimalShelter();
 
-            newAA.Delete(wash);
+            shelter.Load("cat");
+            shelter.Load("cat");
+            shelter.Load("dog");
+            shelter.Load("cat");
+            shelter.Load("dog");
+            shelter.Load("dog");
+            shelter.Load("dog");
+            shelter.Load("cat");
+            shelter.Load("dog");
 
-            newAA.Print();
+            Console.Write($"\nAdopted {shelter.Adopt("cat").Type}.");
+            shelter.Print();
+            Console.WriteLine($"\nAdopted {shelter.Adopt("dog").Type}.");
+            shelter.Print();
+            Console.WriteLine($"\nAdopted {shelter.Adopt("monkey").Type}.");
+            shelter.Print();
+            Console.WriteLine($"\nAdopted {shelter.Adopt("turtle").Type}.");
+            shelter.Print();
+            Console.WriteLine($"\nAdopted {shelter.Adopt("fish").Type}.");
+            shelter.Print();
+
+
+
 
             Console.Read();
         }
