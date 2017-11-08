ae7a858c41bb0c68a014d5d79db54be68e839e91
George Gaylor <georgegaylor83@outlook.com>
2017-11-07 16:21:18 -0800

    Code Cleanup

--- TowerOfHanoi/TowerOfHanoi/Tower.cs
+++ TowerOfHanoi/TowerOfHanoi/Tower.cs
@@ -39,7 +39,7 @@ namespace TowerOfHanoi
                 ShiftOdd();
             }
         }
-
+        
         private void ShiftOdd()
         {
             Right.Push(Left.Pop());
@@ -51,15 +51,17 @@ namespace TowerOfHanoi
                 if (Left.Count != 0 && Right.Count != 0 && Middle.Count == 0)
                     if (Left.Peek().value > Right.Peek().value)
                         Middle.Push(Left.Pop());
-                    else        
+                    else
                         Right.Push(Left.Pop());
                 else if (Right.Count == 0 && Left.Peek().value > Middle.Peek().value)
                     Right.Push(Left.Pop());
                 else if (Left.Count == 0 && Middle.Peek().value < Right.Peek().value)
                     Left.Push(Middle.Pop());
                 else if (Left.Peek().value > Middle.Peek().value && Left.Peek().value > Right.Peek().value)
-                    Middle.Push(Right.Pop());
-
+                    if (Right.Peek().value > Middle.Peek().value)
+                        Right.Push(Middle.Pop());
+                    else
+                        Middle.Push(Right.Pop());
                 else if (Middle.Peek().value < Right.Peek().value)
                     Right.Push(Middle.Pop());
                 else if (Left.Peek().value < Right.Peek().value)
@@ -73,7 +75,7 @@ namespace TowerOfHanoi
 
         private void ShiftEven()
         {
-
+            
         }
 
         public void Print()
