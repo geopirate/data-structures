d60f61656b6e0aaafa369f52a4b0892537a5af81
George Gaylor <georgegaylor83@outlook.com>
2017-11-09 13:16:57 -0800

    Added readme/picture and more code work

--- MiscAlgorithms/PrintLetters/PrintLetters/PrintLetters.cs
+++ MiscAlgorithms/PrintLetters/PrintLetters/PrintLetters.cs
@@ -6,29 +6,46 @@ namespace PrintLetters
 {
     class PrintLetters
     {
+        // static string of all of the letters
         static char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        // the array that will store our letters for the program
         char[] Letters;
+        // the number of letters we are given
         int N;
-        int P = 0;
 
+        // constructor to create the array for manipulation
         public PrintLetters(int n)
         {
             N = n;
+            // creats array of the length of the number given
             Letters = new char[N];
-            foreach(char x in Letters)
+            // puts the corresponding letter in our new array for the affected letters
+            for(int i = 0; i<n; i++)
             {
-                Letters[x] = ALPHABET[x];
+                Letters[i] = ALPHABET[i];
             }
         }
 
-        private void Traversal()
+        public void Traversal()
         {
+            for (int q = N; q > 0; q--)
+            {
 
-        }
+                for (int i = 0; i < q; i++)
+                {
+                    if (q == N)
+                    {
+                        Console.Write($"{new string(Letters)} ");
 
-        public void Print()
-        {
-
+                        continue;
+                    }
+                    char temp = Letters[i];
+                    Letters[i] = Letters[i + 1];
+                    Letters[i + 1] = temp;
+                    Console.Write($"{new string(Letters)} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- MiscAlgorithms/PrintLetters/PrintLetters/Program.cs
+++ MiscAlgorithms/PrintLetters/PrintLetters/Program.cs
@@ -7,6 +7,12 @@ namespace PrintLetters
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int n = 4;
+            PrintLetters MyLetters = new PrintLetters(n);
+            MyLetters.Traversal();
+
+            Console.Read();
         }
     }
 }
