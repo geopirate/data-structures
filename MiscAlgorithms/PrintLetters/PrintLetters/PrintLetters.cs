89cf6ac2b8f3746c2e4eb6254c9190ac9d3deebb
George Gaylor <georgegaylor83@outlook.com>
2017-11-08 23:01:17 -0800

    Initial code

---
+++ MiscAlgorithms/PrintLetters/PrintLetters/PrintLetters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintLetters
+{
+    class PrintLetters
+    {
+        char[] Letters;
+        int N;
+        int P = 0;
+
+        public PrintLetters(string letters)
+        {
+            Letters = letters.ToCharArray();
+            N = letters.Length;
+        }
+
+        private int Traversal()
+        {
+
+        }
+
+        public void Print()
+        {
+
+        }
+    }
+}
---
+++ MiscAlgorithms/PrintLetters/PrintLetters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
