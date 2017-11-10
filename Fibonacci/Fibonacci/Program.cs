d4cb13fcbd3f599396a750446da62156beb43ce6
George Gaylor <georgegaylor83@outlook.com>
2017-11-09 17:27:19 -0800

    Code complete

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -7,6 +7,15 @@ namespace Fibonacci
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // change this to find a different number
+            int n = 4;
+
+            FibonacciFinder fib = new FibonacciFinder();
+            Console.Write($"The {n}th Fibonacci number is ");
+            fib.FindNth(0, 1, n);
+
+
+            Console.Read();
         }
     }
 }
