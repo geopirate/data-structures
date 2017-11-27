41cc09eeee1997d43c903dbfdf9afd71fed1db96
George Gaylor <georgegaylor83@outlook.com>
2017-11-27 01:21:33 -0800

    Created class and readme with print and constructor

---
+++ Matrix/Matrix/Zero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    class Zero
+    {
+        // the matrix to be rotated
+        int[,] matrix;
+        // keeping track of the last index based on n
+        static int m;
+        static int n;
+
+        // constructor that creates the matrix to zero
+        public Zero(int[,] mat)
+        {
+            matrix = mat;
+            n = matrix.GetLength(0);
+            m = matrix.GetLength(1);
+        }
+
+        public void Print()
+        {
+            for (int d = 0; d <= m; d++)
+            {
+                for (int f = 0; f <= n; f++)
+                {
+                    Console.Write($"{matrix[d, f]}, ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Matrix/Matrix/ZeroMatrixReadme.md
@@ -0,0 +1,14 @@
+﻿## What did you learn?
+I learned how to zero out matrix elements.
+## Explain how you felt before starting the challenge, what did you learn while researching, and how well do you understand the data structure after your self research
+I felt this would be a challenging but pretty straightforward data structure to implement as I did the rotation challenge before this.
+## Where did you look?
+I did not do any research, I just dove in.
+### Be specific, what sites? resources? Books?
+See previous answer.
+## What notes did you use to help you successfully complete the challenge?
+Didn't take any notes.
+### Did you write anything down, bookmark anything that looked useful?
+No, see previous answeers.
+## What did you use as a guideline or direction during the challenge?)
+I just used the code.
